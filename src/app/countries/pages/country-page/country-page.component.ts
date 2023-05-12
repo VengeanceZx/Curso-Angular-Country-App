f73b59581d844ec3f7b372ba5b454875ace0b063
Omar Ramos <63936433+Vengenace@users.noreply.github.com>
2023-05-12 17:13:54 -0500

    Fin del desarrollo de la aplicación

---
+++ src/app/countries/pages/country-page/country-page.component.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { CountriesService } from '../../services/countries.service';
+
+import { switchMap} from 'rxjs'
+import { Country } from '../../interfaces/country';
+
+@Component({
+  selector: 'app-country-page',
+  templateUrl: './country-page.component.html',
+  styles: [
+  ]
+})
+export class CountryPageComponent implements OnInit {
+
+  public country? : Country;
+
+  constructor( 
+    private activitedRoute: ActivatedRoute,
+    private router: Router,
+    private countriesService: CountriesService
+    ){}
+
+  ngOnInit(): void {
+    //* this.activitedRoute.params
+    //* .subscribe( ({id}) => {
+      
+    //*   this.countriesService.searchCountryAlphaCode(id)
+    //*   .subscribe( country => {
+    //*     console.log({country});
+        
+    //*   })
+    //* })
+
+    this.activitedRoute.params
+    .pipe(
+      switchMap(({id}) => this.countriesService.searchCountryAlphaCode(id) )
+    )
+    .subscribe( (country)=> { 
+      if (!country) return this.router.navigateByUrl('');
+      return this.country = country;
+      // return;
+    });
+  }
+
+}
